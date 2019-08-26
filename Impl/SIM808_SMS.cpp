da5a9efc87a365a61fe10be680e3e3d9632b5ff7
Victor Cernobai <cernobaiv@gmail.com>
2019-08-26 13:06:28 +0300

    Small fix.

--- Impl/SIM808_SMS.cpp
+++ Impl/SIM808_SMS.cpp
@@ -1,37 +1,8 @@
-/*!
- * @file DFRobot_sim808.cpp
- * @A library  for DFRobot's SIM808 GPS/DFRobot_SIM808/GSM Shield
- *
- * @copyright	[DFRobot](http://www.dfrobot.com), 2016
- *
- * @author [Jason](jason.ling@dfrobot.com)
- * @version  V1.0
- * @date  2016-09-23
- 
- * The MIT License (MIT)
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
- 
+#include "sim808.h"
 #include "SIM808_SMS.h"
 
 
-bool DFRobot_SIM808_SMS::sendSMS(char *number, char *data)
+bool DFRobot_SIM808_SMS::sendSMS(const char* number, const char* data)
 {
     //char cmd[32];
     if(!sim808_check_with_cmd("AT+CMGF=1\r\n", "OK\r\n", CMD)) { // Set message mode to ASCII
